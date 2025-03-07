8daf213226d83d9f0eaa040a349340a4abe3d057
Haze <117860981+HansiFED@users.noreply.github.com>
2025-03-07 14:55:56 +0100

    Checkout page viewported

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -9,11 +9,11 @@ export default function Checkout() {
   }, [clearCart]); // ✅ clearCart is now stable, preventing infinite loop
 
   return (
-    <main className="flex flex-1 items-center flex-col">
-      <img src="/checkout.png" alt="checkout success image" />
-      <h1>Your order has been confirmed!</h1>
+    <main className="flex flex-1 items-center justify-center flex-col text-center gap-5">
+      <img src="/checkout.png" alt="checkout success image" className="w-40" />
+      <h1 className="text-2xl">Your order has been confirmed!</h1>
       <p>An email will be sent to you shortly with your order details and shipping information</p>
-      <p>Your order-id is: #17361927</p>
+      <p>Your order-id is: {`#${Math.floor(Math.random() * 1000000)}`}</p>
     </main>
   );
 }
